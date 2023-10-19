16403b9ef2f075ee52da0b18f680e1cfba8c071f
Mehdi <mehditage10@outlook.fr>
2023-10-19 14:47:30 +0200

    version 1.0.1

---
+++ takeMe-Front/app/screens/connexion.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+
+export const LoginPage: React.FC = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const handleLogin = () => {
+
+    console.log('Email:', email);
+    console.log('Mot de passe:', password);
+  };
+
+  return (
+    <div>
+      <h1>Page de Connexion</h1>
+      <form>
+        <div>
+          <label htmlFor="email">E-mail :</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={handleEmailChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Mot de passe :</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <button type="button" onClick={handleLogin}>
+          Se Connecter
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;

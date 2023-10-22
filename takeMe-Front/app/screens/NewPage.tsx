d6caa4ce790b74ac699684d911c9bfea30d9c1ad
mnadea001 <mathilde.nadeau@hotmail.fr>
2023-10-22 16:25:54 +0200

    WIP

--- takeMe-Front/app/screens/NewPage.tsx
+++ takeMe-Front/app/screens/NewPage.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, ViewStyle, Text } from "react-native";
+import { colors } from "../theme";
+import { Card } from 'react-bootstrap'
 
 export const NewPage = () => {
   return (
-    <View>
-      <Text>Ceci est ma nouvelle page</Text>
-    </View>
+    <React.Fragment>
+        <View style={$container}>
+        <Card style={{ width: 400 }}>
+      <Card.Body>
+        <Card.Title>Test affichageune annonce</Card.Title>
+        <Text>Ceci est ma nouvelle page</Text>
+      </Card.Body>
+    </Card>
+
+      </View>
+    </React.Fragment>
   );
 };
 
+const $container: ViewStyle = {
+  flex: 1,
+  backgroundColor: colors.background,
+  justifyContent: "center",
+  alignItems: "center",
+}
 export default NewPage;

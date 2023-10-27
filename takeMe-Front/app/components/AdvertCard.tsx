dc2e2b22f185c9e960257ffebb3a2004b900d626
mnadea001 <mathilde.nadeau@hotmail.fr>
2023-10-27 16:40:23 +0200

    display image on list

--- takeMe-Front/app/components/AdvertCard.tsx
+++ takeMe-Front/app/components/AdvertCard.tsx
@@ -1,23 +1,25 @@
-import React from "react"
-import { Text, View, TextStyle, ViewStyle, TouchableOpacity } from "react-native"
-import { colors, spacing } from "../theme"
-
+import React from "react";
+import { Text, View, TextStyle, ViewStyle, Image, TouchableOpacity, ImageStyle } from "react-native";
+import { colors, spacing } from "../theme";
 
 interface Props {
-  name: string
-  onPress: (name: string) => void
+  name: string;
+  image: string; // Ajout de la prop image
+  onPress: (name: string) => void;
 }
 
-export const AdvertCard: React.FC<Props> = ({ name, onPress }) => {
+export const AdvertCard: React.FC<Props> = ({ name, image, onPress }) => {
   return (
     <TouchableOpacity onPress={() => onPress(name)}>
       <View style={$container}>
         <View style={$adcard}>
-        <Text style={$name}>{name}</Text>
+          {/* Ajout de l'Image */}
+          <Image source={{ uri: image }} style={$image} />
+          <Text style={$name}>{name}</Text>
         </View>
       </View>
     </TouchableOpacity>
-  )
+  );
 }
 
 const $container: ViewStyle = {
@@ -25,12 +27,14 @@ const $container: ViewStyle = {
   backgroundColor: colors.background,
   padding: 16,
   marginTop: 8,
-}
+};
+
 const $adcard: ViewStyle = {
   padding: spacing.md,
   marginTop: spacing.md,
   minHeight: 120,
-}
+  flexDirection: "row", // Aligner le texte et l'image horizontalement
+};
 
 const $name: TextStyle = {
   fontSize: 16,
@@ -38,7 +42,13 @@ const $name: TextStyle = {
   marginTop: 4,
   color: colors.text,
   backgroundColor: colors.transparent,
+};
 
-}
+const $image: ImageStyle = {
+  width: 100, // Taille de l'image (ajuster selon vos besoins)
+  height: 100, // Taille de l'image (ajuster selon vos besoins)
+  marginRight: 8, // Marge à droite pour l'espace entre l'image et le texte
+};
+
 
 export default AdvertCard

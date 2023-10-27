685ed3ea80a4c0d641406e62d00522be22622f7c
mnadea001 <mathilde.nadeau@hotmail.fr>
2023-10-27 13:55:05 +0200

    WIP

--- takeMe-Front/app/screens/NewPage.tsx
+++ takeMe-Front/app/screens/NewPage.tsx
@@ -7,36 +7,26 @@ import AdvertCard from "app/components/AdvertCard"
 import { NativeStackScreenProps } from "@react-navigation/native-stack"
 import { RootStackParams } from "app/navigators/MenuNavigator"
 import TopDrawerNavigation from "app/components/TopDrawerNavigation"
+import { TouchableOpacity } from "react-native-gesture-handler"
 
 
 
-const image = { uri: 'https://legacy.reactjs.org/logo-og.png' };
+const image = { uri: 'https://images.unsplash.com/photo-1462212210333-335063b676bc?auto=format&fit=crop&q=80&w=1632&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' };
 
 type Props = NativeStackScreenProps<RootStackParams, "NewPage">
 
 export const NewPage = ({ navigation }: Props) => {
   return (
     <React.Fragment>
-         <ImageBackground source={image} resizeMode="cover">
       <View style={$container}>
-        <TopDrawerNavigation />
-     
-          <Card style={{ width: 400 }}>
-            <Card.Body>
-              <Card.Title>Test affichage card</Card.Title>
-              <Text>Ceci est ma nouvelle page</Text>
-            </Card.Body>
-          </Card>
+        <ImageBackground source={image} style={$imagebg}>
+          <TopDrawerNavigation />
 
-          <AdvertCard
-            name="Annonce objet 1"
-            onPress={(name) => {
-              navigation.navigate("AdvertDetailScreen", { name })
-            }}
-          />
-</View>
+          <TouchableOpacity style={$searchBar}>
+            <Text>Le bonheur est dans la rue !</Text>
+          </TouchableOpacity>
         </ImageBackground>
-      
+      </View>
     </React.Fragment>
   )
 }
@@ -44,10 +34,23 @@ export const NewPage = ({ navigation }: Props) => {
 const $container: ViewStyle = {
   flex: 1,
   backgroundColor: colors.background,
-}
-// const $imagebg: ImageStyle = {
-//   height: "100%",
-//   width: "100%",
 
-// }
+}
+const $imagebg: ImageStyle = {
+  height: "100vh",
+  width: "100%",
+
+}
+
+const $searchBar: ImageStyle = {
+  flex: 1,
+  backgroundColor: colors.background,
+  borderRadius: '40px',
+  width: '300px',
+  margin: '20px',
+  padding: '20px',
+  justifyContent: 'center',
+  alignItem: 'center'
+
+}
 export default NewPage

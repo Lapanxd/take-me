23896099207913ebdb70e1604ca8ab9438937e75
mnadea001 <mathilde.nadeau@hotmail.fr>
2023-10-27 17:06:23 +0200

    NNIIICEE All working :D

--- takeMe-Front/app/components/TopDrawerNavigation.tsx
+++ takeMe-Front/app/components/TopDrawerNavigation.tsx
@@ -1,84 +1,73 @@
 import CloseBurgerIcon from 'app/icons/CloseBurgerIcon';
 import React, { useState } from 'react';
-import { Modal, StyleSheet, Pressable, View } from 'react-native';
-import DrawerMenuIcon from '../icons/DrawerMenuIcon'
-import Menu from './Menu'
-
+import { Modal, StyleSheet, Pressable, View, Dimensions } from 'react-native'; // Importez Dimensions depuis 'react-native'
+import DrawerMenuIcon from '../icons/DrawerMenuIcon';
+import Menu from './Menu';
 
 const TopDrawerNavigation = () => {
-    const [modalVisible, setModalVisible] = useState(false);
+  const [modalVisible, setModalVisible] = useState(false);
 
-
-    return <View style={styles.container}>
-        <Modal
-            animationType="fade"
-            transparent={true}
-            visible={modalVisible}
-            onRequestClose={() => { setModalVisible(!modalVisible); }}>
-            <View >
-                <View style={styles.modalView}>
-                    <Menu onClose={() => setModalVisible(false)} /> 
-                    <Pressable
-                        style={styles.button}
-                        onPress={() => setModalVisible(!modalVisible)}>
-                        <CloseBurgerIcon color={'black'} size={40} />
-                    </Pressable>
-                </View>
-            </View>
-        </Modal>
-        <Pressable
-            style={[styles.button, styles.buttonbg]}
-            onPress={() => setModalVisible(true)}>
-            <DrawerMenuIcon color={'black'} size={40} />
-        </Pressable>
+  return (
+    <View style={styles.container}>
+      <Modal
+        animationType="fade" // Changez l'animation en "slide" ou ce que vous préférez
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.modalView}>
+          <Menu onClose={() => setModalVisible(false)} />
+          <Pressable
+            style={styles.button}
+            onPress={() => setModalVisible(!modalVisible)}
+          >
+            <CloseBurgerIcon color={'black'} size={40} />
+          </Pressable>
+        </View>
+      </Modal>
+      <Pressable
+        style={[styles.button, styles.buttonbg]}
+        onPress={() => setModalVisible(true)}
+      >
+        <DrawerMenuIcon color={'black'} size={40} />
+      </Pressable>
     </View>
+  );
 }
 
+const windowWidth = Dimensions.get('window').width; // Obtenez la largeur de la fenêtre
+
 const styles = StyleSheet.create({
-    container: {
-        flexDirection: 'row',
-        margin: 30,
+  container: {
+    flexDirection: 'row',
+    margin: 30,
+  },
+  modalView: {
+    width: windowWidth, // Utilisez la largeur de la fenêtre pour occuper tout l'écran
+    flexDirection: 'row',
+    backgroundColor: 'white',
+    justifyContent: 'flex-end', // Pour positionner le contenu à droite
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
     },
-    backButton: {
-        borderRadius: 8,
-        width: 40,
-        height: 40,
-        justifyContent: "center",
-        alignItems: "center"
-    },
-    modalView: {
-        width: '420px',
-        height: '420px',
-        flexDirection: 'row',
-        backgroundColor: 'white',
-        shadowColor: '#000',
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 4,
-        elevation: 5,
-        padding: 50,
-    },
-    button: {
-        borderRadius: 20,
-        padding: 10,
-        elevation: 2,
-    },
-    buttonbg: {
-        borderRadius: 80,
-        backgroundColor: 'white',
-    },
-    textStyle: {
-        color: 'white',
-        fontWeight: 'bold',
-        textAlign: 'center',
-    },
-    modalText: {
-        textAlign: 'center',
-    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+    padding: 50,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonbg: {
+    borderRadius: 80,
+    backgroundColor: 'white',
+  },
 });
 
-
-export default TopDrawerNavigation
+export default TopDrawerNavigation;

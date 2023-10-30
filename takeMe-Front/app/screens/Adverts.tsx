1a392b4cfc8cfe92dad840a38783311ffd5e5b5e
Mathilde NADEAU <82595811+mnadea001@users.noreply.github.com>
2023-10-30 13:31:06 +0100

    Update Adverts.tsx

--- takeMe-Front/app/screens/Adverts.tsx
+++ takeMe-Front/app/screens/Adverts.tsx
@@ -82,7 +82,11 @@ const markers = [
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-
+    <Marker position={position}>
+      <Popup>
+        A pretty CSS3 popup. <br /> Easily customizable.
+      </Popup>
+    </Marker>
     </MapContainer>
       </View>
       </View>

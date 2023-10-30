e9865924202829052508b05760fc88941a37e3c5
Lapanxd <81854332+Lapanxd@users.noreply.github.com>
2023-10-30 21:20:49 +0100

    Feature/advert api (#1)

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { localConf } from './config/database.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
+import { AuthModule } from './routes/auth/auth.module';
+import { UsersModule } from './routes/users/users.module';
+import { AdvertModule } from './routes/advert/advert.module';
 
 @Module({
-  imports: [AuthModule, TypeOrmModule.forRoot(localConf), UsersModule],
+  imports: [AuthModule, TypeOrmModule.forRoot(localConf), UsersModule, AdvertModule],
   controllers: [],
   providers: [],
 })
---
+++ apps/api/src/core/dtos/advert.dto.ts
@@ -0,0 +1,32 @@
+import { IObjectImage } from '@takeme/models/objectImage.model';
+import { IObjectType } from '@takeme/models/objectType.model';
+import { IsNumber, IsOptional, IsPositive, IsString } from 'class-validator';
+
+export class AdvertDto {
+  @IsOptional()
+  @IsNumber()
+  @IsPositive()
+  id: number;
+
+  @IsOptional()
+  @IsString()
+  name: string;
+
+  @IsOptional()
+  @IsString()
+  description: string;
+
+  @IsOptional()
+  objectType: IObjectType;
+
+  @IsOptional()
+  images: IObjectImage[];
+
+  @IsOptional()
+  @IsNumber()
+  latitude: number;
+
+  @IsOptional()
+  @IsNumber()
+  longitude: number;
+}

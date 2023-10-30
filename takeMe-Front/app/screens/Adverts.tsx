398f9e440ebc6d1cfae97c9baa43bf7d925acf43
mnadea001 <mathilde.nadeau@hotmail.fr>
2023-10-30 19:29:22 +0100

    MAP on list OK

--- takeMe-Front/app/screens/Adverts.tsx
+++ takeMe-Front/app/screens/Adverts.tsx
@@ -10,6 +10,7 @@ import "leaflet/dist/leaflet.css";
 import leaflet from 'leaflet';
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import MarkerClusterGroup from "react-leaflet-cluster";
+import { Icon, divIcon, point } from "leaflet";
 
 type Props = NativeStackScreenProps<RootStackParams, "AdvertsStack">;
 
@@ -35,19 +36,24 @@ export const Adverts = ({ navigation }: Props) => {
 // markers
 const markers = [
   {
-    geocode: [48.86, 2.3522],
+    geocode: [44.8580, -0.5667],
     popUp: "Hello, I am pop up 1"
   },
   {
-    geocode: [48.85, 2.3522],
+    geocode: [44.8375, -0.5667],
     popUp: "Hello, I am pop up 2"
   },
   {
-    geocode: [48.855, 2.34],
+    geocode: [44.8279, -0.5670],
     popUp: "Hello, I am pop up 3"
   }
 ];
-  
+  // create custom icon
+const customIcon = new Icon({
+  iconUrl: "https://cdn-icons-png.flaticon.com/512/447/447031.png",
+//   iconUrl: require("./icons/placeholder.png"),
+  iconSize: [38, 38] // size of the icon
+});
   return (
     <View style={$container}>
       <View style={$header}>
@@ -76,17 +82,24 @@ const markers = [
       </View>
       <View style={$mapcontent}>
       {/* <ScrollView> */}
-      <MapContainer center={[48.8566, 2.3522]} zoom={13} style={{ width: "100%", height: "80vh" }}>
+      <MapContainer center={[44.8378, -0.5667]} zoom={13} style={{ width: "100%", height: "80vh" }}>
       {/* OPEN STREEN MAPS TILES */}
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-    <Marker position={position}>
-      <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
-    </Marker>
+
+<MarkerClusterGroup
+         chunkedLoading
+        //  iconCreateFunction={createClusterCustomIcon}
+       >
+         {/* Mapping through the markers */}
+         {markers.map((marker) => (
+           <Marker position={marker.geocode} icon={customIcon}>
+             <Popup>{marker.popUp}</Popup>
+           </Marker>
+         ))}
+       </MarkerClusterGroup>
     </MapContainer>
       </View>
       </View>
@@ -119,7 +132,7 @@ const $advertslist: ViewStyle = {
 
 const $mapcontent: ViewStyle = {
   flexDirection: "column",
-  width: 300,
+  width: 500,
   height: 300,
 };
 

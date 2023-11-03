fd708133917ea0820bc6c1a4952a7c3f26235efa
Lapanxd <81854332+Lapanxd@users.noreply.github.com>
2023-11-03 15:02:59 +0100

    Fix/packages (#17)

--- packages/front/App.js
+++ packages/front/App.js
@@ -1,19 +1,17 @@
 // This is the entry point if you run `yarn expo:start`
 // If you run `yarn ios` or `yarn android`, it'll use ./index.js instead.
-import App from "./app/app.tsx"
-import React from "react"
-import { registerRootComponent } from "expo"
-import { Platform } from "react-native"
-import * as SplashScreen from "expo-splash-screen"
+import App from './app/app.tsx';
+import React from 'react';
+import { registerRootComponent } from 'expo';
+import * as SplashScreen from 'expo-splash-screen';
 import 'expo-dev-menu';
 
-
-SplashScreen.preventAutoHideAsync()
+SplashScreen.preventAutoHideAsync();
 
 function IgniteApp() {
-  return <App hideSplashScreen={SplashScreen.hideAsync} />
+  return <App hideSplashScreen={SplashScreen.hideAsync} />;
 }
 
-registerRootComponent(IgniteApp)
+registerRootComponent(IgniteApp);
 
-export default IgniteApp
+export default IgniteApp;

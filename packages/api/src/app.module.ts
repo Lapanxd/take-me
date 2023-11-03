63702d26965e5e353af8f122532da09c293944e2
zineeb <mouazzazz@gmail.com>
2023-11-03 11:31:47 +0100

    Fix: Hard-Coded Data and Refresh Token Issue

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import {ConfigModule, ConfigService} from '@nestjs/config';
 import { localConf } from './config/database.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './routes/auth/auth.module';
@@ -6,8 +7,18 @@ import { UsersModule } from './routes/users/users.module';
 import { AdvertModule } from './routes/advert/advert.module';
 
 @Module({
-  imports: [AuthModule, TypeOrmModule.forRoot(localConf), UsersModule, AdvertModule],
-  controllers: [],
-  providers: [],
+    imports: [
+        AuthModule,
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: localConf,
+        }),
+        UsersModule,
+        AdvertModule,
+        ConfigModule.forRoot({isGlobal: true})],
+    controllers: [],
+    providers: [],
 })
-export class AppModule {}
+export class AppModule {
+}
--- packages/api/src/config/database.config.ts
+++ packages/api/src/config/database.config.ts
@@ -4,15 +4,16 @@ import { User } from '../core/entities/user.entity';
 import { ObjectType } from '../core/entities/object-type.entity';
 import { ObjectImage } from '../core/entities/object-image.entity';
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+import { ConfigService } from "@nestjs/config";
 
-export const localConf: TypeOrmModuleOptions = {
+export const localConf = (configService: ConfigService): TypeOrmModuleOptions => ({
   type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'admin',
-  password: 'admin',
-  database: 'takeme',
+  host : configService.get('DB_HOST'),
+  port: +configService.get('DB_PORT'),
+  username: configService.get('DB_USERNAME'),
+  password: configService.get('DB_PASSWORD'),
+  database: configService.get('DB_DATABASE'),
   entities: [User, Advert, ObjectType, ObjectImage],
   namingStrategy: new SnakeNamingStrategy(),
-  synchronize: true,
-};
+  synchronize: configService.get('TYPEORM_SYNC') === 'true',
+});

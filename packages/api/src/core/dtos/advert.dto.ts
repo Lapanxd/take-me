5bc505e85c6be4ea7e62f784394d173a53b651f6
Lapanxd <81854332+Lapanxd@users.noreply.github.com>
2023-11-06 23:19:38 +0100

    Feature/swagger (#19)

--- packages/api/src/core/dtos/advert.dto.ts
+++ packages/api/src/core/dtos/advert.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IObjectImage } from '../models/objectImage.model';
 import { IObjectType } from '../models/objectType.model';
 import { IsNumber, IsOptional, IsPositive, IsString } from 'class-validator';
@@ -6,27 +7,34 @@ export class AdvertDto {
   @IsOptional()
   @IsNumber()
   @IsPositive()
+  @ApiProperty({ example: 1 })
   id: number;
 
   @IsOptional()
   @IsString()
+  @ApiProperty({ example: 'Canapé bleu' })
   name: string;
 
   @IsOptional()
   @IsString()
+  @ApiProperty({ example: 'Un super canapé bleu est plutôt bon état.' })
   description: string;
 
   @IsOptional()
+  @ApiProperty({ example: { id: 1, name: 'meuble' } })
   objectType: IObjectType;
 
   @IsOptional()
+  @ApiProperty({ example: ['url.com/img.png', 'url.com/img2.png'] })
   images: IObjectImage[];
 
   @IsOptional()
   @IsNumber()
+  @ApiProperty({ example: -20 })
   latitude: number;
 
   @IsOptional()
   @IsNumber()
+  @ApiProperty({ example: -10 })
   longitude: number;
 }

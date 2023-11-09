3c40d90b3be7eb85579d883729d513469dd7b65c
mehdit10 <mehditage10@outlook.fr>
2023-11-09 09:19:13 +0100

    connexion_inscription terminé

--- packages/front/app/screens/Connect.tsx
+++ packages/front/app/screens/Connect.tsx
@@ -1,38 +1,71 @@
-import React from 'react';
-import { View, Text, TextInput, StyleSheet, TouchableOpacity, Dimensions, ImageBackground } from 'react-native';
-
+import React, {useState} from 'react';
+import { View, Text, TextInput, StyleSheet, TouchableOpacity, Dimensions, ImageBackground, Image, KeyboardAvoidingView, Platform } from 'react-native';
+import { useNavigation} from '@react-navigation/native';
+import { navigate } from 'app/navigators';
 const background = require('../../assets/images/bg.jpg');
+const logo = require('../../assets/images/logooo.png');
 const screenWidth = Dimensions.get('window').width;
 
 export const Connect = () => {
-  const isMobile = screenWidth <= 768; // Vous pouvez ajuster la largeur limite selon vos besoins
-  const imageWidth = isMobile ? '100%' : screenWidth / 2; // Largeur de l'image
+  const isMobile = screenWidth <= 768;
+  const navigate = useNavigation() as any;  
+
+  const [mail, setMail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  function connexion() 
+  {
+    if (mail === 'user' && password === '123') 
+    {
+      navigate.navigate('Profil', {mail:mail});  
+    } else
+      {
+        setError('Adresse mail ou mot de passe incorrect');    
+      }
+  }
+
+  function inscription()
+  {
+    navigate.navigate('Inscription');
+  }
 
   return (
-    <ImageBackground source={background} style={[styles.container, { width: imageWidth }]}>
-      <View style={[styles.form, isMobile && styles.centerForm]}>
+    <ImageBackground source={background} style={styles.container}>
+      <KeyboardAvoidingView style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+
+  <Image source={logo} style={styles.logo}/>
+      <View style={[styles.form, isMobile && styles.centerForm, isMobile ? styles.centerForm : styles.rightForm]}>
         <Text style={styles.title}>Connexion</Text>
+        <Text style={styles.label}>Adresse e-mail</Text>
         <TextInput
           style={styles.input}
           placeholder="Nom d'utilisateur"
           placeholderTextColor="rgba(128, 128, 128, .5)"
-        />
+          onChangeText={(text) => setMail(text)}
+          />
+        <Text style={styles.label}>Mot de passe</Text>
         <TextInput
           style={styles.input}
           placeholder="Mot de passe"
           placeholderTextColor="rgba(128, 128, 128, .5)"
           secureTextEntry={true}
-        />
-        <TouchableOpacity style={styles.loginButton}>
+          onChangeText={(text) => setPassword(text)}
+          />
+
+          <Text style={styles.error}>{error}</Text>
+        <TouchableOpacity style={styles.loginButton} onPress={connexion}>
           <Text style={styles.buttonText}>Se connecter</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.forgotPassword}>
           <Text style={styles.forgotPasswordText}>Mot de passe oublié ?</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.registerButton}>
+        <TouchableOpacity style={styles.registerButton} onPress={inscription}>
           <Text style={styles.buttonText}>S'inscrire</Text>
         </TouchableOpacity>
       </View>
+      </KeyboardAvoidingView>
     </ImageBackground>
   );
 };
@@ -43,35 +76,65 @@ const styles = StyleSheet.create({
     backgroundColor: '#FFFFFF',
   },
   form: {
-    width: '100%', // Par défaut, prend tout l'écran
-    maxWidth: 400, // Limite la largeur sur les écrans larges (PC)
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    backgroundColor: 'grey',
+    width: '100%',
+    maxWidth: 414,
+    height:'100%',
+    maxHeight: 414,
+    top: '20%',
+    backgroundColor:'rgba(190, 190, 190, .6)',
+    borderRadius:10,
+    alignItems:'center',
   },
   centerForm: {
     alignItems: 'center',
+    justifyContent:'center'
   },
+  rightForm: {
+    marginLeft: 'auto',
+    marginRight: '15%',
+    top:'20%'
+  },
+
+  logo:
+  {
+    marginLeft: 'auto',
+    marginRight: '20%',
+    top:'10%'
+    
+  },
+
   title: {
     fontSize: 24,
     marginBottom: 20,
+    alignSelf:'center',
+    paddingBottom:30,
+    paddingTop:30
   },
+
+  label:
+  {
+    position: 'relative',
+    alignSelf: 'flex-start',
+    marginLeft: '6%'
+  },
+
   input: {
-    width: '100%',
+    width: '90%',
     height: 40,
     borderWidth: 1,
-    borderRadius: 7,
-    padding: 10,
+    borderRadius: 5,
+    padding: 5,
     marginBottom: 10,
+    backgroundColor: ("white")
   },
   loginButton: {
-    backgroundColor: 'blue',
-    width: '50%',
+    backgroundColor: '#212121',
+    width: '60%',
     height: 40,
-    borderRadius: 7,
+    borderRadius: 5,
     justifyContent: 'center',
     alignItems: 'center',
-    alignSelf: 'center',
+    alignSelf:'center'
   },
   buttonText: {
     color: 'white',
@@ -85,14 +148,24 @@ const styles = StyleSheet.create({
   },
   registerButton: {
     backgroundColor: 'green',
-    width: '50%',
+    width: '60%',
     height: 40,
     borderRadius: 5,
     justifyContent: 'center',
     alignItems: 'center',
-    alignSelf: 'center',
+    alignSelf:'center',
     marginTop: 10,
   },
+
+  error:
+  {
+    color: 'red',
+  alignSelf:'flex-start',
+  marginLeft:'6%',
+  
+  marginBottom:5,
+  },
+
 });
 
 export default Connect;
--- packages/front/app/screens/Profil.tsx
+++ packages/front/app/screens/Profil.tsx
@@ -1,596 +1,210 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Image, TextInput, ScrollView, Dimensions, useWindowDimensions, ImageBackground} from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import { useNavigation } from '@react-navigation/native';
-import * as ImagePicker from 'expo-image-picker';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-const defaultUser = require('../../assets/images/default_user.png');
-const logoo = require('../../assets/images/logoo.png');
+import React, {useState} from 'react';
+import { View, Text, TextInput, StyleSheet, TouchableOpacity, Dimensions, ImageBackground, Image } from 'react-native';
+import { useNavigation, useRoute} from '@react-navigation/native';
+import { navigate } from 'app/navigators';
+const background = require('../../assets/images/bg.jpg');
+const logo = require('../../assets/images/logooo.png');
+const screenWidth = Dimensions.get('window').width;
 
-export const Profil = ({ route }) => {
-  const { nom, prenom, username, email, phone } = route.params;
-  const screenWidth = Dimensions.get('window').width;
-  const imageSizeFactor = 0.2;
-  const imageNavSize = 0.1;
-  const logoSize = 0.2;
+export const Profil = () => {
+  const isMobile = screenWidth <= 768;
+  const logoWidth = isMobile ? screenWidth * 0.2 : screenWidth * 0.1; // 20% pour les mobiles, 10% pour les tablettes (ajustez selon vos besoins)
+  const route = useRoute();
+  const userMail = route.params.mail; // Récupérez le mail depuis les paramètres de la route
 
 
-  const windowDimensions = useWindowDimensions();
-  const textSizeFactor = 0.01;
 
+  const navigate = useNavigation() as any;  
 
-  const [nomProfil, setnom] = useState(nom);
-  const [prenomProfil, setPrenom] = useState(prenom);
-  const [usernameProfil, setUsername] = useState(username);
-  const [emailCompte, setEmail] = useState(email);
-  const [phoneCompte, setPhone] = useState(phone);
+  const [nom, setNom] = useState('');
+const [prenom, setPrenom] = useState('');
+const [email, setEmail] = useState(userMail);
+const [password, setPassword] = useState('');
+const [confirmPassword, setConfirmPassword] = useState('');
+const [ville, setVille] = useState('');
 
-  const navigation = useNavigation() as any;
-
-  function Return() {
-    navigation.navigate('Connect');
-  }
-
-  function handleProfil() {
-    navigation.navigate('Profil', { nom, prenom, username, email });
-  }
-
-  const [image, setImage] = useState('');
-
-  const pickImage = async () => {
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
-      allowsEditing: true,
-      aspect: [4, 3],
-      quality: 1,
-    });
-
-    console.log(result);
-
-    if (!result.canceled) {
-      setImage(result.assets[0].uri);
-    }
-  };
-
-  const pickImageCamera = async () => {
-    const cameraPermission = await ImagePicker.requestCameraPermissionsAsync();
-    if (cameraPermission.status === 'granted') {
-      let result = await ImagePicker.launchCameraAsync({
-        allowsEditing: true,
-        aspect: [4, 3],
-        quality: 1,
-      });
-
-      if (!result.canceled) {
-        setImage(result.assets[0].uri);
-      }
-    }
-  };
-
-  const handleLogout = async () => {
-    try {
-      await AsyncStorage.removeItem('authToken');
-      navigation.navigate('Connect');
-    } catch (error) {
-      console.error('Erreur lors de la déconnexion', error);
-    }
-  };
-
-  /*const actions = ['Prendre une nouvelle photo', 'Sélectionner une photo', 'Supprimer la photo', 'Annuler'];
-
-  const showActionSheet = () => {
-    ActionSheetIOS.showActionSheetWithOptions(
-      {
-        options: actions,
-        cancelButtonIndex: actions.length - 1,
-        
-        title: 'Modifier l\'image de profile',
-      },
-      (buttonIndex) => {
-        if (buttonIndex === 0) {
-          pickImageCamera();
-          
-        } else if (buttonIndex === 1) {
-          
-          pickImage();
-          
-        } else if (buttonIndex === 2) {
-          
-          setImage('');
-        }
-
-      }
-    );
-  };
-  */
-
-  //const [selectedOption, setSelectedOption] = useState<'Profil' | 'Securite'>('Profil');
-  const [showOptions, setShowOptions] = useState(false);
 
   return (
-    
     <View style={styles.container}>
-      {/* Barre de navigation */}
       <View style={styles.nav}>
-        <TouchableOpacity style={styles.btnLogo} onPress={Return}>
-          <Image source={logoo} style={[styles.logo, { width: screenWidth * logoSize, height: screenWidth * logoSize }]} resizeMode="contain" />
-        </TouchableOpacity>
+      <Image source={logo} style={styles.logo}/>
 
-        <TextInput
-          style={styles.searchBar}
-          placeholder="Rechercher un objet"
-          placeholderTextColor={'rgba(128, 128, 128, .5)'}
-        ></TextInput>
-
-        <TouchableOpacity style={styles.btnAjout}>
-          <Text style={[styles.txt_ajt_nav, { fontSize: windowDimensions.width * textSizeFactor }]}>Ajouter un objet</Text>
-        </TouchableOpacity>
-
-        {image ? (
-          <Image source={{ uri: image }} style={[styles.profileImage_nav, { width: screenWidth * imageNavSize, height: screenWidth * imageNavSize }]} />
-        ) : (
-          <Image source={defaultUser} style={[styles.profileImage_nav, { width: screenWidth * imageNavSize, height: screenWidth * imageNavSize }]} />
-        )}
-        <TouchableOpacity style={styles.btnMenu} onPress={() => setShowOptions(!showOptions)}>
-          <Icon name="caret-down" size={25} color="black"></Icon>
-        </TouchableOpacity>
-
-        {showOptions && (
-          <View style={styles.optionsContainer}>
-            <TouchableOpacity style={styles.option}>
-              <Text style={[{ fontSize: windowDimensions.width * textSizeFactor }]}>Gérer mes annonces</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity style={styles.optionF}>
-              <Text style={[{ fontSize: windowDimensions.width * textSizeFactor }]}>Mes favoris</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity style={styles.optionC} onPress={handleProfil}>
-              <Text style={[{ fontSize: windowDimensions.width * textSizeFactor }]}>Compte</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity style={styles.option}>
-              <Text style={[{ fontSize: windowDimensions.width * textSizeFactor }]}>Centre d'aide</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity style={styles.option} onPress={handleLogout}>
-              <Text style={[{ fontSize: windowDimensions.width * textSizeFactor }]}>Déconnexion</Text>
-            </TouchableOpacity>
-          </View>
-        )}
+      <TextInput
+          style={styles.inputBar}
+          placeholder="Recherchez un objet"
+          placeholderTextColor="rgba(128, 128, 128, .5)"
+          />
       </View>
-      
-      {/* formulaire Compte & Sécurité */}
-      <View style={styles.formProfil}>  
+      <View style={[styles.form, isMobile && styles.centerForm, isMobile ? styles.centerForm : styles.rightForm]}>
+        <Text style={styles.title}>Vos informations</Text>
 
-        {/* formulaire Profil */}
-          <View style={styles.profilView}>
-            <View style={styles.imgView}>
-              {image ? (
-                <Image source={{ uri: image }} style={[styles.profileImage, { width: screenWidth * imageSizeFactor, height: screenWidth * imageSizeFactor }]}/>
-              ) : (
-                <Image source={defaultUser} style={[styles.profileImage, { width: screenWidth * imageSizeFactor, height: screenWidth * imageSizeFactor }]} />
-              )}
+        <View style={styles.inlineInput}>
+          <View style={styles.inputContainer}>
+        <Text style={styles.label}>Nom</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Nom "
+          placeholderTextColor="rgba(128, 128, 128, .5)"
+          onChangeText={(text) => setNom(text)}
+          />
 
-              <TouchableOpacity style={styles.imgDownload} onPress={pickImage}>
-                <Text style={[styles.txt_btn_img, { fontSize: windowDimensions.width * textSizeFactor }]}>Télécharger</Text>
-              </TouchableOpacity>
-            </View>
-
-            
-              <Text style={[{ fontSize: windowDimensions.width * textSizeFactor }]}>Nom</Text>
-              <TextInput
-                style={styles.input}
-                onChangeText={(text) => setnom(text)}
-                value={nomProfil}
-              />
-
-              <Text style={[{ fontSize: windowDimensions.width * textSizeFactor }]}>Prénom</Text>
-              <TextInput
-                style={styles.input}
-                onChangeText={(text) => setPrenom(text)}
-                value={prenomProfil}
-              />
-
-              <Text style={[{ fontSize: windowDimensions.width * textSizeFactor }]}>Nom d'utilisateur</Text>
-              <TextInput
-                style={styles.input}
-                onChangeText={(text) => setUsername(text)}
-                value={usernameProfil}
-              />
-
-              <Text style={[{ fontSize: windowDimensions.width * textSizeFactor }]}>Email</Text>
-              <TextInput
-                style={styles.input}
-                placeholder="Email"
-                placeholderTextColor="rgba(128, 128, 128, .5)"
-                onChangeText={(text) => setEmail(text)}
-                value={emailCompte}
-              />
-          </View>
-            
-        {/* formulaire Sécurité */}
-          <View style={styles.secuView}>
-            <Text style={styles.txtPass}>Mettre à jours mon mot de passe</Text>
-
-              <Text>Mot de passe</Text>
-              <TextInput
-                style={styles.input}
-                placeholder="********"
-                placeholderTextColor="rgba(128, 128, 128, .5)"
-                onChangeText={(text) => setPhone(text)}
-                value={phoneCompte}
-              />
-
-              <Text>Confirmer le mot de passe</Text>
-              <TextInput
-                style={styles.input}
-                placeholder="********"
-                placeholderTextColor="rgba(128, 128, 128, .5)"
-                onChangeText={(text) => setPhone(text)}
-                value={phoneCompte}
-              />
-            
           </View>
 
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Prénom</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Prénom"
+          placeholderTextColor="rgba(128, 128, 128, .5)"
+          onChangeText={(text) => setPrenom(text)}
+          />
+          </View>
+          </View>
+        <Text style={styles.label}>Adresse e-mail</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="user@gmail.com"
+          placeholderTextColor="rgba(128, 128, 128, .5)"
+          onChangeText={(text) => setEmail(text)}
+          />
+
+        <Text style={styles.label}>Ville</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Bordeaux"
+          placeholderTextColor="rgba(128, 128, 128, .5)"
+          onChangeText={(text) => setVille(text)}
+          />
+
+        
+        <TouchableOpacity style={styles.loginButton}>
+          <Text style={styles.buttonText}>Sauvegarder</Text>
+        </TouchableOpacity>
+        
       </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  /* Conteneur Principal*/
   container: {
     flex: 1,
-    backgroundColor: 'white',
-  },
-
-  /* Barres de navigation*/
-  nav: {
-    position: 'relative',
-    width: '100%',
-    height: '10%',
     backgroundColor: '#FFFFFF',
-    boxShadow: '0px 10px 10px rgba(0, 0, 0, 0.2)',
-    zIndex: 2,
   },
 
-  navForm: {
-    position: 'relative',
-    width: '100%',
-    height: '8%',
-    backgroundColor: '#212121',
-    boxShadow: '0px 10px 10px rgba(0, 0, 0, 0.2)',
-    zIndex: 1,
-  },
-
-  titreNav1: {
-    position: 'absolute',
-    marginLeft: '10%',
-    color: 'white',
-    paddingTop: 15,
-    fontSize: 18,
-  },
-
-  titreNav2: {
-    position: 'absolute',
-    marginLeft: '60%',
-    color: 'white',
-    paddingTop: 15,
-    fontSize: 18,
-  },
-
-  logo: {
-    position: 'absolute',
-    backgroundColor:'grey'
-  },
-
-  /* Formulaires*/
-  formProfil: {
-    flex: 1,
-    flexDirection:'row',
-    justifyContent:'space-between',
-    backgroundColor:'blue'
-   
-  },
-
-  form_tbl_bord: {
-    position: 'absolute',
-    width: '30%',
-    height: '100%',
-    marginTop: '9%',
-  },
-
-  profilView: 
+  nav:
   {
-    flex: 1,
-    marginTop:'10%',
-    paddingRight: 10,
+    position:'absolute',
+    backgroundColor:'grey',
+    width:'100%',
+    height:'10%'
+
   },
 
-  secuView: 
+  inputBar:
   {
-    flex: 1,
-    paddingLeft: 10,
-    marginTop:'29%',
+    position:'absolute',
+    top:'30%',
+    left:'20%',
+    width: '60%',
+    height: '40%',
+    borderRadius:7,
+    backgroundColor:'white'
   },
 
-  section1_tbl_bord: {
+  form: {
     width: '100%',
-    height: '30%',
-    borderBottomWidth: 1,
-    borderBottomColor: '#FF9900',
+    maxWidth: 414,
+    height:'100%',
+    maxHeight: 414,
+    top: '20%',
+    backgroundColor:'rgba(190, 190, 190, .6)',
+    borderRadius:10,
+    alignItems:'center',
+  },
+  centerForm: {
+    alignItems: 'center',
+    justifyContent:'center'
+  },
+  rightForm: {
+    marginLeft: 'auto',
+    marginRight: '15%',
+    top:'20%'
   },
 
-  section2_tbl_bord: {
-    width: '100%',
-    height: '70%',
-    borderBottomWidth: 1,
-  },
-
-  optionsContainer: {
+  logo:
+  {
     position: 'absolute',
-    top: '100%', // Ajustez la position verticale pour aligner les options sous l'icône
-    marginLeft: '87%', // Alignez les options à droite
-    width: '10%',
-    backgroundColor: '#EAEAEA',
-    borderRadius: 7,
-    shadowRadius: 2,
-    zIndex: 4,
+    top:'40%',
+    width: 80,
+    height: 20
+    
   },
 
-  option: {
-    padding: 5, // Ajustez la marge interne pour l'apparence de chaque option
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+    alignSelf:'center',
+    paddingBottom:30,
+    paddingTop:30
   },
 
-  optionF: {
-    padding: 5,
-    borderBottomWidth: 1,
-    borderBottomColor: 'rgba(128, 128, 128, .5)',
-  },
-
-  optionC: {
-    padding: 5,
-    borderBottomWidth: 1,
-    borderBottomColor: 'rgba(128, 128, 128, .5)',
-  },
-
-  /* Textes*/
-  titre_tle_bord: {
-    position: 'absolute',
-    marginTop: '22%',
-    marginLeft: '10%',
-    fontWeight: 'bold',
-    fontSize: 16,
-  },
-
-  btn_pTxt: {
-    fontSize: 20,
-    paddingVertical: 4,
-    color: '#FFFFFF',
-    alignSelf: 'center',
-    justifyContent: 'center',
-  },
-
-  btn_cTxt: {
-    fontSize: 20,
-    paddingVertical: 5,
-    color: '#FFFFFF',
-    alignSelf: 'center',
-    justifyContent: 'center',
-  },
-
-  txt_btn_img: {
-    alignSelf: 'center',
-    justifyContent: 'center',
-    paddingVertical: 5,
-    color: '#FFFFFF',
-  },
-
-  titre: {
-    position: 'absolute',
-    marginTop: '30%',
-    marginLeft: '10%',
-    marginBottom: '5%',
-    fontSize: 16,
-  },
-
-  label: {
-    position: 'absolute',
-    width: 200,
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-
-  txtBtn: {
-    color: 'white',
-    textAlign: 'center',
-  },
-
-  info: {
+  label:
+  {
     position: 'relative',
-    marginLeft: '80%',
-    fontSize: 16,
-    width: 50,
-  },
-
-  txtLogout: {
-    position: 'relative',
-    marginLeft: '25%',
-  },
-
-  txt_section2: {
-    fontSize: 16,
-  },
-
-  txt_ajt_nav: {
-    alignSelf: 'center',
-    justifyContent: 'center',
-    color: '#FFFFFF',
-  },
-
-  txtPass: {
-    position: 'absolute',
-    marginLeft: '20%',
-    fontSize: 18,
-  },
-
-  /* Champs de texte*/
-  searchBar: {
-    position: 'relative',
-    width: '40%',
-    height: '50%',
-    marginTop: 0,
-    marginLeft: '30%',
-    borderRadius: 7,
-    padding: 8,
-    backgroundColor: '#D2D2D2',
+    alignSelf: 'flex-start',
+    marginLeft: '6%'
   },
 
   input: {
-    
+    width: '90%',
+    height: 40,
     borderWidth: 1,
     borderRadius: 7,
-    borderColor: 'rgba(128, 128, 128, .5)',
     padding: 10,
     marginBottom: 10,
-    marginTop: 10,
+    backgroundColor: ("white")
   },
 
-  /* Boutons*/
-  profilBtn: {
-    position: 'relative',
-    marginLeft: '30%',
-    marginTop: '2%',
+  inlineInput: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    left:'5%'
+  },
+  inputContainer: {
+    flex: 1,
+    marginRight: 20,
+    right:'1%'
+  },
+
+  loginButton: {
     backgroundColor: '#212121',
-    borderRadius: 7,
-    width: '12%',
-    height: '5%',
-  },
-
-  btnLogo: {
-    position: 'relative',
-    width: 5,
-    height: 5,
-    marginLeft: 50,
-    marginTop: 15,
-  },
-
-  compteBtn: {
-    position: 'absolute',
-    marginLeft: '60%',
-    marginTop: '2%',
-    backgroundColor: '#212121',
-    borderRadius: 7,
-    width: '12%',
-    height: '5%',
-  },
-
-  logoutBtn: {
-    width: '25%',
-    marginTop: '38%',
-    marginLeft: '10%',
-  },
-
-  logoutIcon: {
-    position: 'absolute',
-  },
-
-  btnMenu: {
-    flex:1,
-    position: 'absolute',
-    marginLeft: '94%',
-    marginTop: '3%',
-  },
-
-  accueilBtn: {
-    position: 'relative',
-    width: '12%',
-    marginTop: '7%',
-    marginLeft: '10%',
-  },
-
-  annonceBtn: {
-    position: 'relative',
-    width: '32%',
-    marginTop: '7%',
-    marginLeft: '10%',
-  },
-
-  ajoutBtn: {
-    position: 'relative',
-    width: '26%',
-    marginTop: '7%',
-    marginLeft: '10%',
-  },
-
-  btnFavori: {
-    position: 'relative',
-    width: '18%',
-    marginTop: '7%',
-    marginLeft: '10%',
-  },
-
-  btnAide: {
-    position: 'relative',
-    width: '21%',
-    marginTop: '7%',
-    marginLeft: '10%',
-  },
-
-  btnAjout: {
-    position: 'absolute',
-    width: '10%',
-    height: '50%',
-    paddingVertical: 9,
-    marginLeft: '72%',
-    marginTop: '1.4%',
-    backgroundColor: '#212121',
-    borderRadius: 7,
-  },
-
-  /* Images & logo*/
-  imgDownload: {
-    backgroundColor: '#212121',
-    width: '15%',
-    height: 30,
-    position: 'absolute',
-    marginLeft: '45%',
-    marginTop: '10%',
+    width: '60%',
+    height: 40,
     borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf:'center'
   },
-
-  profileImage_Bord: {
-    position: 'absolute',
-    width: 70,
-    height: 70,
-    marginTop: '5%',
-    marginLeft: '10%',
-    borderRadius: 100,
-    borderWidth: 5,
-    borderColor: '#FF9900',
+  buttonText: {
+    color: 'white',
   },
-
- 
-
-  imgView: {
-    backgroundColor: 'grey',
+  forgotPassword: {
+    marginTop: 10,
+    alignItems: 'center',
   },
-
-  profileImage: {
-    borderRadius: 100,
-    borderWidth: 5,
-    borderColor: '#FF9900',
+  forgotPasswordText: {
+    color: 'blue',
   },
-
-  profileImage_nav: {
-    position: 'absolute',
-    
-    marginTop: '1%',
-    marginLeft: '90%',
-    borderRadius: 100,
-    borderWidth: 4,
-    borderColor: '#FF9900',
+  registerButton: {
+    backgroundColor: 'green',
+    width: '60%',
+    height: 40,
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf:'center',
+    marginTop: 10,
   },
 });
 

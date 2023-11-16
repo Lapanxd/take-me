ca39827f8520e85bf6fbd9855dd50ab9523c9739
mnadea001 <mathilde.nadeau@hotmail.fr>
2023-11-16 15:14:57 +0100

    WIP

--- packages/front/app/screens/Adverts.tsx
+++ packages/front/app/screens/Adverts.tsx
@@ -1,7 +1,7 @@
 import { NativeStackNavigationProp, NativeStackScreenProps } from '@react-navigation/native-stack';
 import AdvertCard from '../components/AdvertCard';
 import React, { useEffect, useState } from 'react';
-import { View, Text, useWindowDimensions, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, useWindowDimensions, TouchableOpacity, StyleSheet, Picker } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import { RootStackParams } from '../navigators/MenuNavigator';
 import Map from '../components/Map';
@@ -17,12 +17,27 @@ export const Adverts = () => {
   const [adverts, setAdverts] = useState([]);
   const [geocodes, setGeocodes] = useState([]);
   const [names, setNames] = useState([]);
+  const [selectedType, setSelectedType] = useState(null);
+
+  const [filterOptions, setFilterOptions] = useState([]);
 
   useEffect(() => {
     advertService.findAll().then((adverts) => {
       setAdverts(adverts);
       setGeocodes(adverts.map((advert) => advert.geocode));
       setNames(adverts.map((item) => item.name));
+  
+      // Extract unique objectType values
+      const uniqueObjectTypes = Array.from(
+        new Set(adverts.map((advert) => advert.objectType))
+      ).filter(Boolean);
+      
+      // Filter uniqueObjectTypes to include only those with a valid id and name
+      const filteredObjectTypes = uniqueObjectTypes
+        .filter((objectType) => objectType?.id && objectType?.name)
+        .map((objectType) => ({ id: objectType.id, name: objectType.name }));
+      
+      setFilterOptions(filteredObjectTypes);
     });
   }, []);
 
@@ -33,22 +48,40 @@ export const Adverts = () => {
     if (width > 700) return 1;
   }
 
+  const filteredAds = adverts.filter((item) => {
+    if (!selectedType || selectedType === '') {
+      return true;
+    }
+    return item.objectType?.name === selectedType;
+  });
+  
+
   return (
     <>
       <Header />
+      <Picker
+  selectedValue={selectedType}
+  onValueChange={(itemValue) => setSelectedType(itemValue)}
+>
+  <Picker.Item label="Tous les types" value={null} />
+  {filterOptions.map((option) => (
+    <Picker.Item key={option.id} label={option.name} value={option.id} />
+  ))}
+</Picker>
       <View style={{ flexDirection: width > 700 ? 'row' : 'column' }}>
+
         <View style={styles.advertList}>
           <Text style={styles.title}>Toutes les annonces</Text>
           <ScrollView>
             <View style={styles.advertItems}>
-              {adverts.map((advert, index) => (
+            {filteredAds.map((advert, index) => (
                 <View key={index} style={{ width: '50%' }}>
                   <AdvertCard
                     name={advert.name}
                     image={advert.images[0]?.url || 'https://placehold.co/400'}
                     description={advert.description}
                     geocode={advert.geocode}
-                    objectType={advert.objectType}
+                    objectType={advert.objectType ? advert.objectType : { id: null, name: '' }}
                   />
                 </View>
               ))}
